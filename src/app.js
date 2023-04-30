dc2f6c54d36608e7b960b8b5f6ec81a56b977c6e
DEVENDRA MESHRAM <120394539+triggerdeva@users.noreply.github.com>
2023-04-30 19:47:24 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -18,8 +18,19 @@ app.use(express.json());
   ];
 
 //code here 
-
-
-
-
+app.get("/tasks",async (req,res) => {
+   try {
+      res.status(200).json(tasks);
+   } catch (err) {
+      res.status(400).json({ status: "failed",message:err});
+   }
+});
+app.get("/tasks/:id",(req,res) => {
+   if (!isNaN(+req.params.id) && +req.params.id <= 3 && +req.params.id> 0){
+      const data = tasks.find((el) => el.id ===+req.params.id);
+      res.status(200).json(data);
+   }else{
+      res.status(404).send("The task with the provided ID does not exists.");
+   }
+});
 module.exports = app;
