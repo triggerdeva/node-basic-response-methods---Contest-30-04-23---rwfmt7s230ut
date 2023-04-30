78df16efddfc7c417d538d77da91d00f77623c6c
DEVENDRA MESHRAM <120394539+triggerdeva@users.noreply.github.com>
2023-04-30 19:53:16 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,36 +1,38 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 app.use(express.json());
 
-   const tasks =[
-      {
-          id: 1,
-          name:"Jacob"
-      },
-      {
-          id: 2,
-          name:"Mermaid"
-      },
-      {
-          id: 3,
-          name:"Pasto"
-      }
-  ];
+const tasks = [
+  {
+    id: 1,
+    name: "Jacob",
+  },
+  {
+    id: 2,
+    name: "Mermaid",
+  },
+  {
+    id: 3,
+    name: "Pasto",
+  },
+];
 
-//code here 
-app.get("/tasks",async (req,res) => {
-   try {
-      res.status(200).json(tasks);
-   } catch (err) {
-      res.status(400).json({ status: "failed",message:err});
-   }
+//code here
+
+app.get("/tasks", async (req, res) => {
+  try {
+    res.status(200).json(tasks);
+  } catch (err) {
+    res.status(400).json({ status: "failed", message: err });
+  }
 });
-app.get("/tasks/:id",(req,res) => {
-   if (!isNaN(+req.params.id) && +req.params.id <= 3 && +req.params.id> 0){
-      const data = tasks.find((el) => el.id ===+req.params.id);
-      res.status(200).json(data);
-   }else{
-      res.status(404).send("The task with the provided ID does not exists.");
-   }
+app.get("/tasks/:id", (req, res) => {
+  if (!isNaN(+req.params.id) && +req.params.id <= 3 && +req.params.id > 0) {
+    const data = tasks.find((el) => el.id === +req.params.id);
+    res.status(200).json(data);
+  } else {
+    res.status(404).send("The task with the provided ID does not exist.");
+  }
 });
+
 module.exports = app;
